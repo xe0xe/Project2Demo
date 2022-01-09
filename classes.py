46337dbc6a577e3611b0c2fc74e37d23ec0efee5
Artur <asintimur@gmail.com>
2022-01-09 14:29:20 +0300

    Добавлена отрисовка уровня

--- classes.py
+++ classes.py
@@ -1,6 +1,7 @@
 import pygame
 
 pygame.init()
+tile_width = tile_height = 56
 
 
 class Boss(pygame.sprite.Sprite):
@@ -31,40 +32,39 @@ class BossBullet(pygame.sprite.Sprite):
 
     def update(self):
         self.rect.x -= 10
-        if self.rect.x == Bunker().rect.x:
+        if self.rect.x == 6:
             self.rect.x = Boss().rect.x
 
-        elif self.rect.x < Bunker().rect.x:
+        elif self.rect.x < 6:
             self.rect.x = Boss().rect.x
 
 
 class Bunker(pygame.sprite.Sprite):
     image1 = pygame.image.load("data/crateMetal.png")
-    image = pygame.transform.scale(image1, (28, 28))
+    image = pygame.transform.scale(image1, (56, 56))
 
-    def __init__(self, *group):
+    def __init__(self, pos_x, pos_y, *group):
         super().__init__(*group)
         self.image = Bunker.image
         self.rect = self.image.get_rect()
-        self.rect.x = 50
-        self.rect.y = 175
+        self.rect = self.image.get_rect().move(
+            tile_width * pos_x, tile_height * pos_y)
 
 
 class Hero(pygame.sprite.Sprite):
     image1 = pygame.image.load("data/tank_sand.png")
     image = pygame.transform.scale(image1, (42, 46))
 
-    def __init__(self, *group):
+    def __init__(self, pos_x, pos_y, *group):
         super().__init__(*group)
         self.image = Hero.image
         self.rect = self.image.get_rect()
         self.key_is_up = True
-        self.rect.x = 0
-        self.rect.y = 0
+        self.rect = self.image.get_rect().move(
+            tile_width * pos_x, tile_height * pos_y)
 
     def update(self):
         keys = pygame.key.get_pressed()
-        print(self.key_is_up)
         if keys[pygame.K_a]:
             self.rect.x -= 5
         if keys[pygame.K_d]:
@@ -75,9 +75,8 @@ class Hero(pygame.sprite.Sprite):
             self.rect.y += 5
         if keys[pygame.K_LEFT] and self.key_is_up:
             self.image = pygame.transform.rotate(self.image, 90)
-            self.key_is_up = False
         if keys[pygame.K_RIGHT] and self.key_is_up:
             self.image = pygame.transform.rotate(self.image, -90)
-            self.key_is_up = False
+
 
 
--- game.py
+++ game.py
@@ -3,39 +3,102 @@ import os
 import sys
 from classes import Hero, Boss, BossBullet, Bunker
 
-pygame.init()
-size = width, height = 800, 400
+size = width, height = 800, 800
+tile_width = tile_height = 56
 screen = pygame.display.set_mode(size)
 scene = True  # для вызова боса назначить True
 lives = 25  # жизни базы
 
+
+def load_image(name, colorkey=None):
+    fullname = os.path.join('data', name)
+    if not os.path.isfile(fullname):
+        print(f"Файл с изображением '{fullname}' не найден")
+        sys.exit()
+    image = pygame.image.load(fullname)
+    if colorkey is not None:
+        image = image.convert()
+        if colorkey == -1:
+            colorkey = image.get_at((0, 0))
+        image.set_colorkey(colorkey)
+    else:
+        image = image.convert_alpha()
+    return image
+
+
+def load_level(filename):
+    filename = 'data/' + filename
+    # читаем уровень, убирая символы перевода строки
+    with open(filename, 'r') as mapFile:
+        level_map = [line.strip() for line in mapFile]
+
+    # и подсчитываем максимальную длину
+    max_width = max(map(len, level_map))
+
+    # дополняем каждую строку пустыми клетками ('.')
+    return list(map(lambda x: x.ljust(max_width, '.'), level_map))
+
+
+def generate_level(level):
+    new_player, x, y = None, None, None
+    for y in range(len(level)):
+        for x in range(len(level[y])):
+            if level[y][x] == '.':
+                Tile('empty', x, y)
+            elif level[y][x] == '#':
+                Tile('wall', x, y)
+            elif level[y][x] == '@':
+                Tile('empty', x, y)
+                new_player = Hero(x, y, player_group, all_sprites)
+            elif level[y][x] == 'b':
+                Bunker(x, y, all_sprites)
+    # вернем игрока, а также размер поля в клетках
+    return new_player, x, y
+
+
+class Tile(pygame.sprite.Sprite):
+    def __init__(self, tile_type, pos_x, pos_y):
+        super().__init__(tiles_group, all_sprites)
+        self.type = tile_type
+        image1 = pygame.transform.scale(tile_images[tile_type], (56, 56))
+        self.image = image1
+        self.rect = self.image.get_rect().move(
+            tile_width * pos_x, tile_height * pos_y)
+
+
 if __name__ == '__main__':
     running = True
     all_sprites = pygame.sprite.Group()
+    tiles_group = pygame.sprite.Group()
+    player_group = pygame.sprite.Group()
 
+
+    tile_images = {
+        'wall': load_image('crateWood.png'),
+        'empty': load_image('tileGrass1.png')
+    }
     screen.fill(pygame.Color('white'))
     FPS = 60
     clock = pygame.time.Clock()
-
-    player = Hero(all_sprites)
-    Bunker(all_sprites)
+    player, level_x, level_y = generate_level(load_level('level.txt'))
 
     if scene:  # проверка начата ли сцена с босом
         Boss(all_sprites)
         BossBullet(all_sprites)
     while running:
-        clock.tick(FPS)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
             if event.type == pygame.KEYUP:
                 player.key_is_up = True
-                print('yes i am here')
+
         screen.fill(pygame.Color('white'))
 
-        all_sprites.draw(screen)
         all_sprites.update()
-
+        tiles_group.draw(screen)
+        all_sprites.draw(screen)
+        player_group.draw(screen)
+        clock.tick(FPS)
         pygame.display.flip()
 
     pygame.quit()
